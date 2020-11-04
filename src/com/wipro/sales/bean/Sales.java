23e32b559b8273ad7ca38840d6fb6d57fa7671eb
Shivam1432 <69252067+Shivam1432@users.noreply.github.com>
2020-11-04 15:11:12 +0530

    Create Sales.java

---
+++ src/com/wipro/sales/bean/Sales.java
@@ -0,0 +1,40 @@
+package com.wipro.sales.bean;
+
+public class Sales {
+	private String salesID;
+	private java.util.Date salesDate;
+	private String productID;
+	private int quantitySold;
+	private double salesPricePerUnit;
+	public String getSalesID() {
+		return salesID;
+	}
+	public void setSalesID(String salesID) {
+		this.salesID = salesID;
+	}
+	public java.util.Date getSalesDate() {
+		return salesDate;
+	}
+	public void setSalesDate(java.util.Date salesDate) {
+		this.salesDate = salesDate;
+	}
+	public String getProductID() {
+		return productID;
+	}
+	public void setProductID(String productID) {
+		this.productID = productID;
+	}
+	public int getQuantitySold() {
+		return quantitySold;
+	}
+	public void setQuantitySold(int quantitySold) {
+		this.quantitySold = quantitySold;
+	}
+	public double getSalesPricePerUnit() {
+		return salesPricePerUnit;
+	}
+	public void setSalesPricePerUnit(double salesPricePerUnit) {
+		this.salesPricePerUnit = salesPricePerUnit;
+	}
+	
+}
